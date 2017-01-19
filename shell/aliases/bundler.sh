26170d4ce5922da833fdfd92d3f836a318ac21cf
Marty Zalega <marty@zalega.me>
2017-01-20 08:31:13 +1000

    Check bundler is installed prior to aliasing

--- shell/aliases/bundler.sh
+++ shell/aliases/bundler.sh
@@ -1,4 +1,6 @@
-alias be='bundle exec'
-alias bi='bundle install'
-alias bu='bundle update'
-alias bo='bundle open'
+if [ command -v bundle >/dev/null 2>&1 ]; then
+  alias be='bundle exec'
+  alias bi='bundle install'
+  alias bu='bundle update'
+  alias bo='bundle open'
+fi
