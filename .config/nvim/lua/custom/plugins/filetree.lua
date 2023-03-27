95432c194093616ff66c32332dacd041dc3f3258
Marty Zalega <martyz@canva.com>
2023-03-28 08:22:07 +1000

    Update nvim settings based off kickstart.nvim

---
+++ .config/nvim/lua/custom/plugins/filetree.lua
@@ -0,0 +1,20 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function ()
+    require('neo-tree').setup {}
+    vim.cmd([[ nnoremap / :Neotree toggle current reveal_force_cwd<cr> ]])
+    vim.cmd([[ nnoremap \| :Neotree toggle reveal<cr> ]])
+    vim.cmd([[ nnoremap gd :Neotree float reveal_file=<cfile> reveal_force_cwd<cr> ]])
+    vim.cmd([[ nnoremap <leader>b :Neotree toggle show buffers right<cr> ]])
+    vim.cmd([[ nnoremap <leader>s :Neotree float git_status<cr> ]])
+  end,
+}
